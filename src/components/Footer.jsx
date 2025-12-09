249e767351bc78237b3b4ad0308dab4f1070af1e
Pat <dmtpatricica@email.com>
2025-12-09 21:32:17 +0100

    modified css  + footer finished + improved header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,35 +2,85 @@
 function Footer() {
     return (
         <footer>
-            <div id="footerMainLinks">
-                <div>
-                <img src="./src/assets/img/buy-comics-digital-comics.png" alt="" />
-                <p>DIGITAL COMICS</p>
-                </div>
-                <div>
-                <img src="./src/assets/img/buy-comics-merchandise.png" alt="" />
-                <p>DC MERCHANDISE</p>
-                </div>
-                <div>
-                <img src="./src/assets/img/buy-comics-subscription.png" alt="" />
-                <p>SUBSCRIPTION</p>
-                </div>
-                <div>
-                <img src="./src/assets/img/buy-comics-shop-locator.png" alt="" />
-                <p>COMIC SHOP LOCATOR</p>
-                </div>
-                <div>
-                <img src="./src/assets/img/buy-dc-power-visa.svg" alt="" />
-                <p>DC POWER VISA</p>
-                </div>
+        <div className="footerMainLinks">
+            <div className="flexContainer boxed flexBetween flexCenterItems">
+                
+                    <img src="./src/assets/img/buy-comics-digital-comics.png" alt="" />
+                    <p>DIGITAL COMICS</p>
+               
+                    <img src="./src/assets/img/buy-comics-merchandise.png" alt="" />
+                    <p>DC MERCHANDISE</p>
+               
+                    <img src="./src/assets/img/buy-comics-subscriptions.png" alt="" />
+                    <p>SUBSCRIPTION</p>
+                
+                    <img src="./src/assets/img/buy-comics-shop-locator.png" alt="" />
+                    <p>COMIC SHOP LOCATOR</p>
+                
+                    <img src="./src/assets/img/buy-dc-power-visa.svg" alt="" />
+                    <p>DC POWER VISA</p>
+                
             </div>
-            <div id="footerLinks">
-                Links
+        </div>
+        <div className="boxed footerLinks flexContainer flexBetween">
+            <div className="flexContainer fRow">
+                <ul>
+                    <h3>DC COMICS</h3>
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">News</a></li>
+
+                    <h3>SHOP</h3>
+                    <li><a href="#">Shop DC</a></li>
+                    <li><a href="#">Shop DC Collectibles</a></li>
+                </ul>
+                <ul>
+                    <h3>DC</h3>
+                    <li><a href="#">Terms of Use</a></li>
+                    <li><a href="#">Privacy policy (New)</a></li>
+                    <li><a href="#">Ad Choices</a></li>
+                    <li><a href="#">Advertising</a></li>
+                    <li><a href="#">Jobs</a></li>
+                    <li><a href="#">Subscriptions</a></li>
+                    <li><a href="#">Talent Workshops</a></li>
+                    <li><a href="#">CPSC Certificates</a></li>
+                    <li><a href="#">Ratings</a></li>
+                    <li><a href="#">Shop Help</a></li>
+                    <li><a href="#">Contact Us</a></li>
+
+                </ul>
+                <ul>
+                    <h3>SITES</h3>
+                    <li><a href="#">DC</a></li>
+                    <li><a href="#">MAD Magazine</a></li>
+                    <li><a href="#">DC Kids</a></li>
+                    <li><a href="#">DC Universe</a></li>
+                    <li><a href="#">Dc Power Visa</a></li>
+
+                </ul>
             </div>
-            <div id="footerSocialBar">
-                Social
+            <img className="fLinkImage" src="./src/assets/img/dc-logo-bg.png" alt="" srcset="" />
+        </div>
+        <div className="footerSocialBar ">
+            <div className="flexContainer boxed fEnd flexCenterItems">
+                <button className="fSignUpBtn">
+                    <h3>SIGN-UP NOW!</h3>
+                </button>
+                <a href="#">
+                    <h3>FOLLOW US</h3>
+                </a>
+                <a href="#"><img src="./src/assets/img/footer-facebook.png" alt="" /></a>
+                <a href="#"><img src="./src/assets/img/footer-twitter.png" alt="" /></a>
+                <a href="#"><img src="./src/assets/img/footer-youtube.png" alt="" /></a>
+                <a href="#"><img src="./src/assets/img/footer-pinterest.png" alt="" /></a>
+                <a href="#"><img src="./src/assets/img/footer-periscope.png" alt="" /></a>
             </div>
-        </footer>
+        </div>
+    </footer>
     )
 }
 export default Footer
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,40 @@
 
 function Header() {
     return (
-        <header>
-            <div>
-
-            <img src="./src/assets/img/dc-logo.png" alt="" srcset="" />
-            <ul id="navMenu">
-                <li><a href="#">CHARACTERS</a></li>
-                <li><a href="#">COMICS</a></li>
-                <li><a href="#">MOVIES</a></li>
-                <li><a href="#">TV</a></li>
-                <li><a href="#">GAMES</a></li>
-                <li><a href="#">COLLECTABLES</a></li>
-                <li><a href="#">FANS</a></li>
-                <li><a href="#">NEWS</a></li>
-                <li><a href="#">SHOP</a></li>
+        <header className="flexContainer flexCenterItems">
+        <div className="flexContainer boxed flexCenterItems flexBetween">
+            <img src="./src/assets/img/dc-logo.png" className="fStart" alt="" srcset="" />
+            <ul id="navMenu" >
+                <div>
+                    <li><a href="#">CHARACTERS</a></li>
+                </div>
+                <div>
+                    <li><a href="#">MOVIES</a></li>
+                </div>
+                <div>
+                    <li><a href="#">COMICS</a></li>
+                </div>
+                <div>
+                    <li><a href="#">TV</a></li>
+                </div>
+                <div>
+                    <li><a href="#">GAMES</a></li>
+                </div>
+                <div>
+                    <li><a href="#">COLLECTABLES</a></li>
+                </div>
+                <div>
+                    <li><a href="#">FANS</a></li>
+                </div>
+                <div>
+                    <li><a href="#">NEWS</a></li>
+                </div>
+                <div>
+                    <li><a href="#">SHOP</a></li>
+                </div>
             </ul>
-            </div>
-        </header>
+        </div>
+    </header>
     )
 }
 export default Header
