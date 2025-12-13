ef83bb6ed5300e799f464e3fe47aa8e71a1c47e8
Pat <dmtpatricica@email.com>
2025-12-13 15:33:15 +0100

    footer social bar component created and linked

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import './Footer.css'
 import Minitron from './footer_components/Minitron.components.jsx';
 import FooterMain from './footer_components/FooterMain.components.jsx';
+import FooterSocial from './footer_components/FooterSocial.components.jsx';
 
 
 function Footer() {
@@ -10,21 +11,7 @@ function Footer() {
             {/********************links*********>^.^<***********************/}
             <FooterMain />
             {/********************social*********>^.^<***********************/}
-            <div className="footerSocialBar ">
-                <div className="flexContainer boxed fEnd flexCenterItems">
-                    <button className="fSignUpBtn">
-                        <h3>SIGN-UP NOW!</h3>
-                    </button>
-                    <a href="#">
-                        <h3>FOLLOW US</h3>
-                    </a>
-                    <a href="#"><img src="./src/assets/img/footer-facebook.png" alt="" /></a>
-                    <a href="#"><img src="./src/assets/img/footer-twitter.png" alt="" /></a>
-                    <a href="#"><img src="./src/assets/img/footer-youtube.png" alt="" /></a>
-                    <a href="#"><img src="./src/assets/img/footer-pinterest.png" alt="" /></a>
-                    <a href="#"><img src="./src/assets/img/footer-periscope.png" alt="" /></a>
-                </div>
-            </div>
+            <FooterSocial />
         </footer>
     )
 }
---
+++ src/components/footer_components/FooterSocial.components.jsx
@@ -0,0 +1,21 @@
+
+function FooterSocial() {
+    return (
+        <div className="footerSocialBar ">
+            <div className="flexContainer boxed fEnd flexCenterItems">
+                <button className="fSignUpBtn">
+                    <h3>SIGN-UP NOW!</h3>
+                </button>
+                <a href="#">
+                    <h3>FOLLOW US</h3>
+                </a>
+                <a href="#"><img src="./src/assets/img/footer-facebook.png" alt="" /></a>
+                <a href="#"><img src="./src/assets/img/footer-twitter.png" alt="" /></a>
+                <a href="#"><img src="./src/assets/img/footer-youtube.png" alt="" /></a>
+                <a href="#"><img src="./src/assets/img/footer-pinterest.png" alt="" /></a>
+                <a href="#"><img src="./src/assets/img/footer-periscope.png" alt="" /></a>
+            </div>
+        </div>
+    )
+}
+export default FooterSocial;
