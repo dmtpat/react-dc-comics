46efcfd103a7c510186ab4437142025c79bcd940
Pat <dmtpatricica@email.com>
2025-12-13 12:35:13 +0100

    footer separated minitron + minor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,10 @@
 import './Footer.css'
+import Minitron from './Minitron.components'
 
 function Footer() {
     return (
         <footer>
-        <div className="footerMainLinks">
-            <div className="flexContainer boxed flexBetween flexCenterItems">
-                
-                    <img src="./src/assets/img/buy-comics-digital-comics.png" alt="" />
-                    <p>DIGITAL COMICS</p>
-               
-                    <img src="./src/assets/img/buy-comics-merchandise.png" alt="" />
-                    <p>DC MERCHANDISE</p>
-               
-                    <img src="./src/assets/img/buy-comics-subscriptions.png" alt="" />
-                    <p>SUBSCRIPTION</p>
-                
-                    <img src="./src/assets/img/buy-comics-shop-locator.png" alt="" />
-                    <p>COMIC SHOP LOCATOR</p>
-                
-                    <img src="./src/assets/img/buy-dc-power-visa.svg" alt="" />
-                    <p>DC POWER VISA</p>
-                
-            </div>
-        </div>
+        <Minitron />
         <div className="boxed footerLinks flexContainer flexBetween">
             <div className="flexContainer fRow">
                 <ul>
---
+++ src/components/Minitron.components.jsx
@@ -0,0 +1,26 @@
+
+function Minitron() {
+    return (
+        <div className="footerMainLinks">
+            <div className="flexContainer boxed flexBetween flexCenterItems">
+                
+                    <img src="./src/assets/img/buy-comics-digital-comics.png" alt="" />
+                    <p>DIGITAL COMICS</p>
+               
+                    <img src="./src/assets/img/buy-comics-merchandise.png" alt="" />
+                    <p>DC MERCHANDISE</p>
+               
+                    <img src="./src/assets/img/buy-comics-subscriptions.png" alt="" />
+                    <p>SUBSCRIPTION</p>
+                
+                    <img src="./src/assets/img/buy-comics-shop-locator.png" alt="" />
+                    <p>COMIC SHOP LOCATOR</p>
+                
+                    <img src="./src/assets/img/buy-dc-power-visa.svg" alt="" />
+                    <p>DC POWER VISA</p>
+                
+            </div>
+        </div>
+    )
+}
+export default Minitron
