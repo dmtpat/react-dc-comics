e3034ebc399d2dac4010d03573390f83281e7807
Pat <dmtpatricica@email.com>
2025-12-13 12:34:15 +0100

    header separated data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,55 @@
 import './Header.css'
+import  navMenuLinks  from './data/navMenuLinks.components.jsx';
+// const navMenuLinks = [
+//     { id: 1, label: 'CHARACTERS', href: '#' },
+//     { id: 2, label: 'MOVIES', href: '#' }, 
+//     { id: 3, label: 'COMICS', href: '#' },
+//     { id: 4, label: 'TV', href: '#' },
+//     { id: 5, label: 'GAMES', href: '#' },
+//     { id: 6, label: 'COLLECTABLES', href: '#' },
+//     { id: 7, label: 'FANS', href: '#' },
+//     { id: 8, label: 'NEWS', href: '#' },
+//     { id: 9, label: 'SHOP', href: '#' }
+// ];
 
 function Header() {
     return (
         <header className="flexContainer flexCenterItems">
         <div className="flexContainer boxed flexCenterItems flexBetween">
             <img src="./src/assets/img/dc-logo.png" className="fStart" alt="" srcset="" />
-            <ul id="navMenu" >
-                <div>
-                    <li><a href="#">CHARACTERS</a></li>
+                <ul id="navMenu" >
+                    {navMenuLinks.map((link) => (
+                        <div key={link.id}>
+                            <li><a href={link.href}>{link.label}</a></li>
+                        </div>
+                    ))} 
+                {/* <div>
+                    <li key={id}><a href="#">CHARACTERS</a></li>
                 </div>
                 <div>
-                    <li><a href="#">MOVIES</a></li>
+                    <li key={id}><a href="#">MOVIES</a></li>
                 </div>
                 <div>
-                    <li><a href="#">COMICS</a></li>
+                    <li key={id}><a href="#">COMICS</a></li>
                 </div>
                 <div>
-                    <li><a href="#">TV</a></li>
+                    <li key={id}><a href="#">TV</a></li>
                 </div>
                 <div>
-                    <li><a href="#">GAMES</a></li>
+                    <li key={id}><a href="#">GAMES</a></li>
                 </div>
                 <div>
-                    <li><a href="#">COLLECTABLES</a></li>
+                    <li key={id}><a href="#">COLLECTABLES</a></li>
                 </div>
                 <div>
-                    <li><a href="#">FANS</a></li>
+                    <li key={id}><a href="#">FANS</a></li>
                 </div>
                 <div>
-                    <li><a href="#">NEWS</a></li>
+                    <li key={id}><a href="#">NEWS</a></li>
                 </div>
                 <div>
-                    <li><a href="#">SHOP</a></li>
-                </div>
+                    <li key={id}><a href="#">SHOP</a></li>
+                </div> */}
             </ul>
         </div>
     </header>
---
+++ src/components/data/navMenuLinks.components.jsx
@@ -0,0 +1,13 @@
+
+const navMenuLinks = [
+    { id: 1, label: 'CHARACTERS', href: '#' },
+    { id: 2, label: 'MOVIES', href: '#' }, 
+    { id: 3, label: 'COMICS', href: '#' },
+    { id: 4, label: 'TV', href: '#' },
+    { id: 5, label: 'GAMES', href: '#' },
+    { id: 6, label: 'COLLECTABLES', href: '#' },
+    { id: 7, label: 'FANS', href: '#' },
+    { id: 8, label: 'NEWS', href: '#' },
+    { id: 9, label: 'SHOP', href: '#' }
+];
+export default navMenuLinks;
