87b405e2a46f2e5e54f63e5d4b0c93ae1d990e2e
Pat <dmtpatricica@email.com>
2025-12-13 15:20:54 +0100

    FooterMain components created and linked

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import './Footer.css'
-import Minitron from './Minitron.components.jsx';
-import footerLinks from './data/footerMainLinks.components.jsx';
+import Minitron from './footer_components/Minitron.components.jsx';
+import FooterMain from './footer_components/FooterMain.components.jsx';
 
 
 function Footer() {
@@ -8,83 +8,7 @@ function Footer() {
         <footer>
             <Minitron />
             {/********************links*********>^.^<***********************/}
-            <div className="boxed footerLinks flexContainer flexBetween">
-                <div className="flexContainer fRow">
-                    {footerLinks.map(function (section, index) {
-                        if (section.section === 'DC COMICS') {
-                            return (
-                                <div key='firstRow'>
-                                    <ul key={section.section + 1}>
-                                        <li key={section.section}><h3>{section.section}</h3></li>
-                                        {section.links.map((link) => (
-                                            <li key={link.id}><a href={link.href}>{link.label}</a></li>
-                                        ))}
-                                    </ul>
-                                    <ul key={section.section + 2}>
-                                        <li key='shop'><h3>SHOP</h3></li>
-                                        {footerLinks[1].links.map((link) => (
-                                            <li key={link.id}><a href={link.href}>{link.label}</a></li>
-                                        ))}
-                                    </ul>
-                                </div>)
-                        } else {
-                            if (section.section === 'SHOP') { }
-                            else {
-                                return (
-                                    <ul key={index}>
-                                        <li key={section.section}><h3>{section.section}</h3></li>
-                                        {section.links.map((link) => (
-                                            <li key={link.id}><a href={link.href}>{link.label}</a></li>
-                                        ))}
-                                    </ul>)
-                            }
-                        }
-                    }
-                    )}
-                    {/* <div>
-                        <ul>
-                            <li key={id}><h3>DC COMICS</h3></li>
-                            <li key={id}><a href="#">Characters</a></li>
-                            <li key={id}><a href="#">Comics</a></li>
-                            <li key={id}><a href="#">Movies</a></li>
-                            <li key={id}><a href="#">TV</a></li>
-                            <li key={id}><a href="#">Games</a></li>
-                            <li key={id}><a href="#">Videos</a></li>
-                            <li key={id}><a href="#">News</a></li>
-                        </ul>
-                        <ul>
-                            <li key={id}><h3>SHOP</h3></li>
-                            <li key={id}><a href="#">Shop DC</a></li>
-                            <li key={id}><a href="#">Shop DC Collectibles</a></li>
-                        </ul>
-                    </div>
-                    <ul>
-                        <li key={id}><h3>DC</h3></li>
-                        <li key={id}><a href="#">Terms of Use</a></li>
-                        <li key={id}><a href="#">Privacy policy (New)</a></li>
-                        <li key={id}><a href="#">Ad Choices</a></li>
-                        <li key={id}><a href="#">Advertising</a></li>
-                        <li key={id}><a href="#">Jobs</a></li>
-                        <li key={id}><a href="#">Subscriptions</a></li>
-                        <li key={id}><a href="#">Talent Workshops</a></li>
-                        <li key={id}><a href="#">CPSC Certificates</a></li>
-                        <li key={id}><a href="#">Ratings</a></li>
-                        <li key={id}><a href="#">Shop Help</a></li>
-                        <li key={id}><a href="#">Contact Us</a></li>
-
-                    </ul>
-                    <ul>
-                        <li key={id}><h3>SITES</h3></li>
-                        <li key={id}><a href="#">DC</a></li>
-                        <li key={id}><a href="#">MAD Magazine</a></li>
-                        <li key={id}><a href="#">DC Kids</a></li>
-                        <li key={id}><a href="#">DC Universe</a></li>
-                        <li key={id}><a href="#">Dc Power Visa</a></li>
-
-                    </ul> */}
-                </div>
-                <img className="fLinkImage" src="./src/assets/img/dc-logo-bg.png" alt="" srcset="" />
-            </div>
+            <FooterMain />
             {/********************social*********>^.^<***********************/}
             <div className="footerSocialBar ">
                 <div className="flexContainer boxed fEnd flexCenterItems">
---
+++ src/components/footer_components/FooterMain.components.jsx
@@ -0,0 +1,43 @@
+import footerLinks from '../data/footerMainLinks.components.jsx';
+
+function footerMain() {
+    return (
+        <div className="boxed footerLinks flexContainer flexBetween">
+            <div className="flexContainer fRow">
+                {footerLinks.map(function (section, index) {
+                    if (section.section === 'DC COMICS') {
+                        return (
+                            <div key='firstRow'>
+                                <ul key={section.section + 1}>
+                                    <li key={section.section}><h3>{section.section}</h3></li>
+                                    {section.links.map((link) => (
+                                        <li key={link.id}><a href={link.href}>{link.label}</a></li>
+                                    ))}
+                                </ul>
+                                <ul key={section.section + 2}>
+                                    <li key='shop'><h3>SHOP</h3></li>
+                                    {footerLinks[1].links.map((link) => (
+                                        <li key={link.id}><a href={link.href}>{link.label}</a></li>
+                                    ))}
+                                </ul>
+                            </div>)
+                    } else {
+                        if (section.section === 'SHOP') { }
+                        else {
+                            return (
+                                <ul key={index}>
+                                    <li key={section.section}><h3>{section.section}</h3></li>
+                                    {section.links.map((link) => (
+                                        <li key={link.id}><a href={link.href}>{link.label}</a></li>
+                                    ))}
+                                </ul>)
+                        }
+                    }
+                }
+                )}
+            </div>
+            <img className="fLinkImage" src="./src/assets/img/dc-logo-bg.png" alt="" srcset="" />
+        </div>
+    )
+}
+export default footerMain   
