0c97158e90f6407f5c54e55f7597ab53cbf55310
Pat <dmtpatricica@email.com>
2025-12-13 15:12:25 +0100

    footer main links data separated

---
+++ src/components/data/footerMainLinks.components.jsx
@@ -0,0 +1,46 @@
+const footerLinks = [{
+    section: 'DC COMICS',
+    links:
+        [
+            { id: 1, label: 'Characters', href: '#' },
+            { id: 2, label: 'Comics', href: '#' },
+            { id: 3, label: 'Movies', href: '#' },
+            { id: 4, label: 'TV', href: '#' },
+            { id: 5, label: 'Games', href: '#' },
+            { id: 6, label: 'Videos', href: '#' },
+            { id: 7, label: 'News', href: '#' }
+        ]
+
+}, {
+    section: 'SHOP',
+    links: [
+        { id: 1, label: 'Shop DC', href: '#' },
+        { id: 2, label: 'Shop DC Collectibles', href: '#' }
+    ]
+}, {
+    section: 'DC',
+    links: [
+        { id: 1, label: 'Terms of Use', href: '#' },
+        { id: 2, label: 'Privacy policy (New)', href: '#' },
+        { id: 3, label: 'Ad Choices', href: '#' },
+        { id: 4, label: 'Advertising', href: '#' },
+        { id: 5, label: 'Jobs', href: '#' },
+        { id: 6, label: 'Subscriptions', href: '#' },
+        { id: 7, label: 'Talent Workshops', href: '#' },
+        { id: 8, label: 'CPSC Certificates', href: '#' },
+        { id: 9, label: 'Ratings', href: '#' },
+        { id: 10, label: 'Shop Help', href: '#' },
+        { id: 11, label: 'Contact Us', href: '#' }
+    ]
+}, {
+    section: 'SITES',
+    links: [
+        { id: 1, label: 'DC', href: '#' },
+        { id: 2, label: 'MAD Magazine', href: '#' },
+        { id: 3, label: 'DC Kids', href: '#' },
+        { id: 4, label: 'DC Universe', href: '#' },
+        { id: 5, label: 'Dc Power Visa', href: '#' }
+    ]
+}
+];
+export default footerLinks;
